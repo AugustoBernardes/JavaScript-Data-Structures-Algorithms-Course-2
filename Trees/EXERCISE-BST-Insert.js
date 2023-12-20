cafe81fcfac2682074aea226325f1d68979ce023
Augusto Bernardes <augustobdev@outlook.com>
2023-12-20 08:59:47 -0300

    feat: saving the progress about trees

---
+++ Trees/EXERCISE-BST-Insert.js
@@ -0,0 +1,76 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+ 
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+	insert(value){
+        const newNode = new Node(value)
+
+        if(!this.root) return this.root = newNode
+        let temp = this.root
+
+        while(temp){
+            if(newNode.value === temp.value) return undefined
+
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode
+                }
+                temp = temp.left
+            }else {
+                if(temp.right === null){
+                    temp.right = newNode
+                }
+                temp = temp.right
+            }
+        }
+    }
+
+}
+
+
+
+function test() {
+
+    let myBST = new BST();
+
+    myBST.insert(2);
+    myBST.insert(1);
+    myBST.insert(3);
+
+    /*
+        THE LINES ABOVE CREATE THIS TREE:
+                     2
+                    / \
+                   1   3
+    */
+
+
+    console.log("Root:", myBST.root.value);
+    console.log("\nRoot->Left:", myBST.root.left.value);
+    console.log("\nRoot->Right:", myBST.root.right.value);
+}
+
+
+test();
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Root: 2
+
+    Root->Left: 1
+
+    Root->Right: 3
+
+*/
+      
